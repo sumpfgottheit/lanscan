ccd44759d9b309c70f1d3e34ef51bc0c5812c126
Florian Sachs <florian.sachs@gmx.at>
2016-11-17 19:02:39 +0100

    The Networks / Network object is in the middle..

--- lanscan/lanscan.py
+++ lanscan/lanscan.py
@@ -14,7 +14,9 @@ import scapy.layers.l2
 import scapy.route
 import socket
 import math
+import sys
 import re
+import netaddr
 import errno
 import click
 from os.path import realpath, basename, isdir
@@ -23,6 +25,9 @@ import netifaces
 logging.basicConfig(format='%(asctime)s %(levelname)-5s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+def exit_n(message, exitcode=1):
+    click.echo("{} {}".format(click.style('Failed', fg='red'), message), err=True)
+    sys.exit(exitcode)
 
 def long2net(arg):
     if (arg <= 0 or arg >= 0xFFFFFFFF):
@@ -61,6 +66,105 @@ def scan_and_print_neighbors(net, interface, timeout=1):
             raise
 
 
+def get_driver(name):
+    try:
+        d = basename(realpath('/sys/class/net/{}/device/driver'.format(name)))
+        if d == 'driver':
+            return ''
+        else:
+            return d
+    except Exception:
+        return ''
+
+
+def get_hardware(driver):
+    if driver == '':
+        return ''
+    try:
+        r = subprocess.check_output(['modinfo', driver]).decode('utf-8')
+        return re.search(r'^description:\s*(.*)', r, re.M).groups()[0]
+    except Exception:
+        return ''
+
+
+class Networks:
+    def __init__(self):
+        self.networks = []
+
+    def initialize(self):
+        interfaces = {}
+
+        try:
+            default_gateway_ip, default_interface_name = netifaces.gateways()['default'][netifaces.AF_INET]
+        except Exception:
+            default_gateway_ip, default_interface_name = None, None
+
+        for interface_name in netifaces.interfaces():
+
+            if interface_name not in interfaces:
+                __driver = get_driver(interface_name)
+                __hardware = get_hardware(__driver)
+                interfaces[interface_name] = {'driver': __driver, 'hardware': __hardware}
+
+            if netifaces.AF_INET in netifaces.ifaddresses(interface_name):
+                for network in netifaces.ifaddresses(interface_name)[netifaces.AF_INET]:
+                    ipnet = netaddr.IPNetwork("{}/{}".format(network['addr'], network['netmask']))
+                    network_ip = ipnet.network
+                    netmask = ipnet.netmask
+                    prefix = ipnet.prefixlen
+                    driver = interfaces[interface_name]['driver']
+                    hardware = interfaces[interface_name]['hardware']
+
+                    is_default_network = interface_name == default_interface_name and default_gateway_ip in ipnet
+
+                    self.networks.append(
+                        Network(interface_name, network_ip, netmask, prefix, driver, hardware, is_default_network))
+
+    @property
+    def len(self):
+        return len(self.networks)
+
+    @property
+    def default_network_id(self):
+        for i, network in enumerate(self.networks):  # type: Network
+            if network.is_default_network:
+                return i
+
+    @property
+    def default_network(self):
+        return self.networks[self.default_network_id]
+
+    def get_network_for_netaddr_ip(self, netaddr_ip: netaddr.IPNetwork):
+        for network in self.networks:   # type: Network
+            if network.netaddr_ip == netaddr_ip:
+                return network
+        else:
+            raise KeyError("No local network for {} found.".format(str(netaddr_ip.cidr)))
+
+
+class Network:
+    def __init__(self, interface_name, network_ip, netmask, prefix, driver, hardware, is_default_network):
+        self.interface_name = interface_name
+        self.network_ip = network_ip
+        self.netmask = netmask
+        self.prefix = prefix
+        self.driver = driver
+        self.hardware = hardware
+        self.is_default_network = is_default_network
+        self.sort_value = netaddr.IPAddress(self.network_ip).value
+        self.netaddr_ip = netaddr.IPNetwork("{}/{}".format(self.network_ip, self.netmask))
+
+    @property
+    def cidr(self):
+        return str(self.netaddr_ip.cidr)
+
+    def __repr__(self):
+        return ("<Network:{s.network_ip}, Netmask:{s.netmask}, Prefix:{s.prefix}, "
+                "Default:{s.is_default_network}, Interface_Name:{s.interface_name}, "
+                "Driver:{s.driver}, Hardware:{s.hardware}, "
+                "sort_value:{s.sort_value}>").format(s=self)
+
+
 class Interface():
     @classmethod
     def get_driver(cls, name):
@@ -84,13 +188,13 @@ class Interface():
             return ''
 
     def __init__(self, name, i):
-        self.i = i
+        self.i = i[netifaces.AF_INET]
         self.name = name
         self.driver = Interface.get_driver(name)
         self.hardware = Interface.get_hardware(self.driver)
 
     def __repr__(self):
-        return "<Name:{s.name},Driver:{s.driver},Hardware:{s.hardware}".format(s=self)
+        return "<Name:{s.name},Driver:{s.driver},Hardware:{s.hardware},i:{s.i}".format(s=self)
 
 
 class Route:
@@ -134,28 +238,42 @@ def routes():
 @click.group('main')
 @click.pass_context
 def main(ctx):
-    _routes = routes()
-    _interfaces = []
-    for i in netifaces.interfaces():
-        if netifaces.AF_INET in netifaces.ifaddresses(i):
-            _interfaces.append(Interface(i, netifaces.ifaddresses(i)))
-    ctx.obj = {'routes': _routes, 'interfaces': _interfaces}
+    networks = Networks()
+    networks.initialize()
+    ctx.obj = {'networks': networks}
 
 
-@main.command('interfaces')
-@click.pass_context
-def interfaces(ctx):
-    o = ctx.obj
-    for interface in o['interfaces']:
-        print(interface)
+@main.command('networks', help='Display a list of available networks.')
+@click.pass_obj
+def networks(o):
+    for i, network in enumerate(o['networks'].networks, start=1):  # type: Network
+        first = '*' if network.is_default_network else ' '
+        print("{}: {} {}/{} on {}".format(i, first, network.network_ip, network.prefix, network.interface_name))
 
 
-@main.command('scan')
-@click.pass_context
-def scan(ctx):
-    o = ctx.obj
-    for route in o['routes']:  # type: Route
-        net = "{}/{}".format(route.net, route.mask)
-        scan_and_print_neighbors(net, route.iface)
-        # if net:
-        #    scan_and_print_neighbors(net, interface)
+@main.command('scan', help='Scan a network, defaults to default network.')
+@click.option('--network', '-n', 'arg_network', required=False,
+              help="The network to scan in CIDR notation or the network number from 'lanscan networks'")
+@click.pass_obj
+def scan(o, arg_network):
+    networks = o['networks']    # type: Networks
+    n = None
+    if arg_network is None:
+        n = networks.default_network
+    else:
+        try:
+            i = int(arg_network)
+            if 1 <= i <= len(networks.networks):
+                n = networks.networks[i - 1]
+            else:
+                exit_n("No network for id {} found. Try 'lanscan networks' to get networks.".format(i))
+        except ValueError:
+            try:
+                _n = netaddr.IPNetwork(arg_network)
+                click.echo("Scan for: {}".format(_n.cidr))
+                n = networks.get_network_for_netaddr_ip(_n)
+            except (KeyError, netaddr.AddrFormatError) as e:
+                exit_n(str(e))
+
+    click.echo("Network: {}".format(n.cidr))
+
